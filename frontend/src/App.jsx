29453809205608c4984f720c7eaeb94606c23c0c
Thulasi <thulasipanndeshwara02@gmail.com>
2025-05-13 10:47:10 +0530

    litt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,20 @@ import VolunteerDashboard from './modules/Dashboard/VolunteerDashboard';
 import EventCard from './modules/Events/EventCard';
 import EventForm from './modules/Events/EventForm';
 import EventList from './modules/Events/EventList';
-import ShareEvent.jsx from './modules/'
+import ShareEvent from './modules/Events/ShareEvent';
+import AchievementsPage from './modules/Gamification/AchievementsPage';
+import BadgeCard from './modules/Gamification/BadgeCard';
+import LeaderboardPage from './modules/Gamification/LeaderboardPage';
+import ActivityLog from './modules/Participation/ActivityLog';
+import CertificatePage from './modules/Participation/CertificatePage';
+import FeedbackPage from './modules/Participation/FeedbackPage';
+import MyEventsPage from './modules/Participation/MyEventPage';
+import HomePage from './modules/Profile/HomePage';
+import Profile from './modules/Profile/Profile';
 
 import CertificatePage from "./modules/Participation/CertificatePage";
+
+import Navbar from './components/Navbar';
 
 
 
@@ -30,7 +41,7 @@ import CertificatePage from "./modules/Participation/CertificatePage";
 // import BadgeCard from './pageandcards/BadgeCard';
 // import LeaderboardPage from './pageandcards/LeaderboardPage';
 // import AchievementsPage from './pageandcards/AchievementsPage';
-import Navbar from './components/Navbar';
+
 
 function App() {
   return (
@@ -43,19 +54,30 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/eventform" element={<EventForm />} />
-        <Route path="/eventlist" element={<EventList />} />
+
         <Route path="/volunteerdashboard" element={<VolunteerDashboard />} />
         <Route path="/sidebar" element={<Sidebar />} />
+
+        
+        <Route path="/eventform" element={<EventForm />} />
+        <Route path="/eventlist" element={<EventList />} />
         <Route path="/eventcard" element={<EventCard />} />
-        <Route path="/myeventpage" element={<MyEventsPage />} />
-        <Route path="/eventcards" element={<EventCards />} />
-        <Route path="/feedbackpage" element={<FeedbackPage />} />
-        <Route path="/activitylog" element={<ActivityLog />} />
+        <Route path="/shareevent" element={<ShareEvent />} />
+
         <Route path="/badgecard" element={<BadgeCard />} />
         <Route path="/leaderboardpage" element={<LeaderboardPage />} />
         <Route path="/achievementspage" element={<AchievementsPage />} />
+
+
+        <Route path="/myeventpage" element={<MyEventsPage />} />
+        <Route path="/certificatepage" element={<CertificatePage />} />
+        <Route path="/feedbackpage" element={<FeedbackPage />} />
+        <Route path="/activitylog" element={<ActivityLog />} />
+
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/profile" element={<Profile />} />
+        
+
       </Routes>
     </Router>
   );
